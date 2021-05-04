b6ea02f87faf9a2f39e16bb68ad6d21552ce827f
Igor Mutschaeweski <igor.mutschaeweski@gmail.com>
2021-05-03 22:06:49 -0300

    Exercicio resolvido de matriz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de linhas e colunas para a matriz:");
+            String[] y = Console.ReadLine().Split(' ');
+
+            Console.WriteLine();
+
+            int m = int.Parse(y[0]);
+            int n = int.Parse(y[1]);
+
+            int[,] matriz = new int[m,n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(valores[j]);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Digite um numero da matriz:");
+            int x = int.Parse(Console.ReadLine());
+
+         //   Console.WriteLine();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i, j] == x)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Posição na matriz: " + "[" + i + "," + j + "]");
+                        
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Numero à esquerda: " + matriz[i,j-1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Numero acima: " + matriz[i-1,j]);
+                        }                     
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Numero à direita: " + matriz[i,j+1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Numero abaixo: " + matriz[i+1,j]);
+                        }
+                    
+                    }
+
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
